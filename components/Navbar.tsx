fa0df64fa2dccfe4c81758d9e0eb58a14e82dc2c
sradnan <adnan.taskco@gmail.com>
2026-06-16 00:40:01 +0600

    all updated code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -117,7 +117,7 @@ export default function Navbar() {
               </Link>
 
               <Link
-                href="#"
+                href="/loginpage"
                 className="flex items-center gap-2 hover:text-primary"
               >
                 <FiUser />
